8bd7307ccbb0f554dc4e8facabfa832508eedf1f
KshithijSinghania <kshithijsinghania@gmail.com>
2025-07-16 17:57:12 +0530

    fixed SocketContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/userContext.jsx";
 import CaptainContext from "./context/captainContext.jsx";
-import SocketProvider from "./context/socketContext.jsx";
+import SocketProvider from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <CaptainContext>
